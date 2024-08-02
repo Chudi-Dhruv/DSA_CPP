a9019c5c5ec72fcc25e7139be3fe389f118116ac
Chudi-Dhruv <124705493+Chudi-Dhruv@users.noreply.github.com>
2024-08-02 21:17:11 +0530

    Tabulation method for KnapSack01_2d_DP.cpp

--- KnapSack01_2d_DP.cpp
+++ KnapSack01_2d_DP.cpp
@@ -33,12 +33,37 @@ int solveMem(vector<int> &values, vector<int> &weights, int i, int w, vector<vec
 	
 	return dp[i][w];
 }
+int solveTab( vector<int> &weight, vector<int> &value, int n, int capacity ) {
+	// create a dp array
+	vector<vector<int> > dp(n, vector<int>(capacity+1, 0) );
+	// base case analysis
+	for( int w = weight[0]; w <= capacity; w++ ){
+		if(weight[0] <= capacity )
+			dp[0][w] = value[0];
+		else
+			dp[0][w] = 0;
+	}
+	
+	for( int index = 1; index < n; index++ ) {
+		for( int w = 0; w <= capacity; w++ ) {
+			int include = 0;
+			
+			if( weight[index] <= w )
+				include = value[index] + dp[index-1][w-weight[index]];
+			int exclude = 0 + dp[index-1][w];
+			
+			dp[index][w] = max( exclude, include );
+		}
+	}
+	return dp[n-1][capacity];
+}
 
 
 int maxProfit(vector<int> &values, vector<int> &weights, int n, int w)
 {
 	vector<vector<int>> dp(n,vector<int>((w+1),-1));
-	return solveMem(values,weights, n-1 ,w, dp);
+	return solveTab(weights,values, n,w);
+	// return solveMem(values,weights, n-1 ,w, dp);
 	// return solve(values,weights, n-1 ,w);
 	// Write your code here
 }
